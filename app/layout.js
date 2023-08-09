27d19eaf2c6d14cfd7aa9c2865ddbf09fe5f2cd6
Arafatulla <sharullmusicplaylist@gmail.com>
2023-08-09 13:08:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
   title: "Sharull | Portfolio | Frontend developer",
   description:
     "Explore a captivating collection of frontend projects that showcase technical prowess. Browse the best of my work, and envision how together, we can bring your next big idea to life. Let's collaborate and turn dreams into reality. Contact me today and let's embark on a transformative creative partnership!",
+  verification: {
+    google: "Kn-9zZ3xqj-Oa7BYWCABVXxW5RgXc57Si0Kk9SCW4YA"
+  }
 };
 
 export default function RootLayout({ children }) {
